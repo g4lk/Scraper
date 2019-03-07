67a0f017eb0b12b4ec15841c8da222f8e361a30f
PDParla <pd.parla@alumnos.urjc.es>
2019-03-07 12:26:42 -0500

    config atras

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
-class Periodicos():
-    def __init__(self):
-        self.periodicos = {}
-        with open('periodicos.txt','r') as f:
-            urls = f.read().split()
-            for url in urls:
-                self.periodicos[url] = False
 
 class Config():
-    def __init__(self, user='URJCReadWrite',password='UniversidadReyJuanCarlos2019', direction='127.0.0.1', port=27017 ):
+    def __init__(self, user='xxxx',password='xxxx', direction='127.0.0.1', port=27017 ):
         self.user = user
         self.password = password
         self.direction = direction
