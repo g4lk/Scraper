3d3334f28876d2e0ccf0008cd95bf015d885b6e1
g4lk <pd.parla@alumnos.urjc.es>
2019-03-08 22:01:24 +0100

    Mas periodicos, programa ahora acepta lista de palabras, no elegir una si no quieres

--- prueba.py
+++ prueba.py
@@ -6,22 +6,19 @@ from pymongo import MongoClient
 from urllib.parse import urlsplit, urlunsplit,urlparse
 import urllib.parse, argparse, datetime, sys, os, ssl
 
-#p = Periodicos()
 conf = Config()
 client = MongoClient(username=conf.user, password= conf.password, host=conf.direction, port=conf.port)
 
-def guardarEnBBDD(url,texto):
+def guardarEnBBDD(url,texto,palabras):
 
     global p
     global conf
     global client
-    urlLocal = urlsplit(url)
-    p.periodicos[f'{urlLocal.scheme}://{urlLocal.netloc}/'] = True
-    post = {"url"  : f"{url}",
-            "title": f"{texto.get('title')}",
-            "text" : f"{texto.get('text')}",
-        #    "point": f"{punto}",
-            "date" : datetime.datetime.utcnow()}
+    post = {"url"  : url,
+            "title": texto.get('title'),
+            "text" : texto.get('text'),
+            "date" : datetime.datetime.utcnow(),
+            "words": ",".join(palabras)}
     db = client.Noticias
     noticia = db.noticia
     post_id = noticia.insert_one(post).inserted_id
@@ -54,15 +51,25 @@ def recolectarTexto(url):
     return {'title': a.title, 'text': a.text}
 
 def fixURL(url, href):
+    getElem = href.find("#")
     if 'http://' in href.lower() or 'https://' in href.lower():
-        return href
+        if getElem != -1:
+            return href[:getElem]
+        else:
+            return href
     elif 'http:/' in href.lower() or 'https:/' in href.lower():
-        return f'{url}{href[href.find("/",7)+1:]}'
+        if getElem != -1:
+            return f'{url}{href[href.find("/",7)+1:getElem]}'
+        else:
+            return f'{url}{href[href.find("/",7)+1:]}'
     else:
-        return urllib.parse.urljoin(url, href)
+        if getElem != -1:
+            return urllib.parse.urljoin(url, href[:getElem])
+        else:
+            return urllib.parse.urljoin(url, href)
 
 
-def elegirNoticia(url,palabra):
+def elegirNoticia(url,palabras):
     header = {'User-Agent' :'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     req = urllib.request.Request(url, headers=header)
     if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
@@ -74,50 +81,52 @@ def elegirNoticia(url,palabra):
     lista = []
     urls = set()
     for link in soup.find_all('a'):
-        if palabra.lower() in str(link).lower() and not urllib.parse.urljoin(url,str(link.get('href'))) in urls:
-            lista.append(link)
-            if link.get('href') != None:
-                urls.add(fixURL(url,str(link.get('href').replace(" ",""))))
+        for palabra in palabras:
+            urlfixed = fixURL(url,str(link.get('href').replace(" ","")))
+            if palabra.lower() in str(link).lower() and not urlfixed in urls:
+                lista.append(link)
+                if link.get('href') != None:
+                    urls.add(urlfixed)
     if len(lista) > 1:
         lista = list(set(lista))
         for index, element in enumerate(lista):
             print(f'{index}: {element}\n')
         try:
-            inp = int(input('Selecciona un número de los anteriores: '))
+            inp = int(input('Selecciona un número de los anteriores o -1 si no quieres ninguno: '))
 
             if inp in range(0, len(lista)):
                 return fixURL(url,str(lista[inp].get('href')).replace(" ", ""))
+            elif inp == -1:
+                print(f'No hay noticias que te satisfazcan en {url}, vamos al siguiente')
             else:
                 print('El numero no está en rango,saliendo')
                 sys.exit(1)
-        except KeyboardInterrupt:
+        except Exception:
             print('Siguiente periodico')
-
     elif len(lista) == 1:
         return fixURL(url,str(lista[0].get('href')).replace(" ", ""))
     else:
-        print(f'No hay noticias con la palabra {palabra} en {url}, vamos al siguiente')
+        print(f'No hay noticias con las palabras {",".join(palabras)} en {url}, vamos al siguiente')
 
-def analizarTextos(palabra):
+def analizarTextos(palabras):
     with open('periodicos.txt','r') as f:
         urls = f.read().split()
         for url in urls:
-            urlNoticia = elegirNoticia(url,palabra)
+            urlNoticia = elegirNoticia(url,palabras)
             if (urlNoticia):
                 texto = recolectarTexto(urlNoticia)
-    #            punto = emocionesTexto(texto.get(text))
                 if (texto.get('text') != ''):
-                    guardarEnBBDD(urlNoticia , texto)
+                    guardarEnBBDD(urlNoticia , texto, palabras)
 
 def parseArg():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w','--word', help='Word to search')
+    parser.add_argument('-w','--words', help='Words to search')
     args = parser.parse_args()
-    return args.word
+    return args.words.split(",")
 
 def main():
-    palabraaBuscar = parseArg()
-    analizarTextos(palabraaBuscar)
+    palabrasaBuscar = parseArg()
+    analizarTextos(palabrasaBuscar)
 
 if __name__ == '__main__':
     main()
