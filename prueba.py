1fc9085273677161e4e555f4b8e8816328fe930b
g4lk <pd.parla@alumnos.urjc.es>
2019-03-14 13:32:00 +0100

    Comentado y mejorado, dependencias en fichero

--- prueba.py
+++ prueba.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from newspaper import Article
 from config import Config
 from pymongo import MongoClient
-from urllib.parse import urlsplit, urlunsplit,urlparse
+from urllib.parse import urlsplit
 import urllib.parse, argparse, datetime, sys, os, ssl
 
 conf = Config()
@@ -11,20 +11,21 @@ client = MongoClient(username=conf.user, password= conf.password, host=conf.dire
 
 def guardarEnBBDD(url,texto,palabras):
 
-    global p
-    global conf
+    # Creamos el diccionario (json) que introduciremos en la base de datos
     global client
     post = {"url"  : url,
             "title": texto.get('title'),
             "text" : texto.get('text'),
             "date" : datetime.datetime.utcnow(),
             "words": ",".join(palabras)}
+    # Creamos la conexion a la base de datos y la coleccion que utilizaremos e insertamos
     db = client.Noticias
     noticia = db.noticia
     post_id = noticia.insert_one(post).inserted_id
     print(f'Insertada nueva noticia con id: {post_id}, con url: {url}')
 
 def getTexto(url):
+    # Recogemos el texto (sin saltos de linea) cuando no puede automaticamente
     print(f'Porfavor, introduce el texto de la noticia desde {url} sin saltos de linea. No he sido capaz de descargarlo yo:\n')
     content = []
     while True:
@@ -37,7 +38,7 @@ def getTexto(url):
 
 def recolectarTexto(url):
     try:
-
+        # Recogemos automaticamente noticia y titulo
         a = Article(url)
         a.download()
         a.parse()
@@ -45,12 +46,14 @@ def recolectarTexto(url):
             a.text = getTexto(url)
         print('\n')
     except Exception:
+        # Si no se puede lo hacemos en modo manual
         a.text = getTexto(url)
         a.title = input('Introduce el titulo de la noticia: ')
 
     return {'title': a.title, 'text': a.text}
 
 def fixURL(url, href):
+    # Parseamos la url para comprobación de duplicados y correcion de errores
     getElem = href.find("#")
     if 'http://' in href.lower() or 'https://' in href.lower():
         if getElem != -1:
@@ -70,23 +73,28 @@ def fixURL(url, href):
 
 
 def elegirNoticia(url,palabras):
+    #Creamos headers y un contexto de ssl para evitar errores o bloqueos
     header = {'User-Agent' :'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     req = urllib.request.Request(url, headers=header)
     if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
             getattr(ssl, '_create_unverified_context', None)):
             ssl._create_default_https_context = ssl._create_unverified_context
+    # Hacemos la peticion y parseamos el html
     page = urlopen(req)
     html = page.read()
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser',from_encoding="iso-8859-1")
+    # Guardamos en una lista todos los tags a que tengan una de las palabras metidas en args
+    # y guardamos un conjunto de urls para comprobar duplicados de url
     lista = []
     urls = set()
     for link in soup.find_all('a'):
-        for palabra in palabras:
-            urlfixed = fixURL(url,str(link.get('href').replace(" ","")))
-            if palabra.lower() in str(link).lower() and not urlfixed in urls:
-                lista.append(link)
-                if link.get('href') != None:
+            if link.get('href'):
+                urlfixed = fixURL(url,str(link.get('href').replace(" ","")))
+            # Si hay alguna palabra en el link y no está ya guardada la metemos
+                if any(palabra in str(link).lower() for palabra in palabras) and not urlfixed in urls:
+                    lista.append(link)
                     urls.add(urlfixed)
+    # Si tenemos solo un atributo lo guardamos, si tenemos mas de uno elegimos cual guardar, y si no, no queremos ninguno.
     if len(lista) > 1:
         lista = list(set(lista))
         for index, element in enumerate(lista):
@@ -109,6 +117,7 @@ def elegirNoticia(url,palabras):
         print(f'No hay noticias con las palabras {",".join(palabras)} en {url}, vamos al siguiente')
 
 def analizarTextos(palabras):
+    # Por cada periodico elegimos una noticia y recopilamos en base de datos
     with open('periodicos.txt','r') as f:
         urls = f.read().split()
         for url in urls:
@@ -119,6 +128,7 @@ def analizarTextos(palabras):
                     guardarEnBBDD(urlNoticia , texto, palabras)
 
 def parseArg():
+    # Recogemos argumentos
     parser = argparse.ArgumentParser()
     parser.add_argument('-w','--words', help='Words to search')
     args = parser.parse_args()
